e201dd2470d6e666e69c00e127b1d9d3e7211a6c
Gabriel Richard <87505238+gabrielrichard01@users.noreply.github.com>
2023-12-12 15:32:55 -0300

    Update PostComments.test.tsx

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,28 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    test('Adicionar 2 comentarios', () => {
+        render(<PostComment/>)
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId(comment-button))
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
+    });
+})
